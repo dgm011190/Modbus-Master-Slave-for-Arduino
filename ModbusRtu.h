71d4fc574ba948328b92963e9de9009a036da8c7
dgm011190 <62054084+dgm011190@users.noreply.github.com>
2020-03-11 03:17:14 -0600

    Update ModbusRtu.h

--- ModbusRtu.h
+++ ModbusRtu.h
@@ -93,132 +93,7 @@ enum MESSAGE
  * @see also modbus_t
  */
 enum MB_FC
-{
-    MB_FC_NONE                     = 0,   /*!< null operator */
-    MB_FC_READ_COILS               = 1,	/*!< FCT=1 -> read coils or digital outputs */
-    MB_FC_READ_DISCRETE_INPUT      = 2,	/*!< FCT=2 -> read digital inputs */
-    MB_FC_READ_REGISTERS           = 3,	/*!< FCT=3 -> read registers or analog outputs */
-    MB_FC_READ_INPUT_REGISTER      = 4,	/*!< FCT=4 -> read analog inputs */
-    MB_FC_WRITE_COIL               = 5,	/*!< FCT=5 -> write single coil or output */
-    MB_FC_WRITE_REGISTER           = 6,	/*!< FCT=6 -> write single register */
-    MB_FC_WRITE_MULTIPLE_COILS     = 15,	/*!< FCT=15 -> write multiple coils or outputs */
-    MB_FC_WRITE_MULTIPLE_REGISTERS = 16	/*!< FCT=16 -> write multiple registers */
-};
-
-enum COM_STATES
-{
-    COM_IDLE                     = 0,
-    COM_WAITING                  = 1
-
-};
-
-enum ERR_LIST
-{
-    ERR_NOT_MASTER                = -1,
-    ERR_POLLING                   = -2,
-    ERR_BUFF_OVERFLOW             = -3,
-    ERR_BAD_CRC                   = -4,
-    ERR_EXCEPTION                 = -5
-};
-
-enum
-{
-    NO_REPLY = 255,
-    EXC_FUNC_CODE = 1,
-    EXC_ADDR_RANGE = 2,
-    EXC_REGS_QUANT = 3,
-    EXC_EXECUTE = 4
-};
-
-const unsigned char fctsupported[] =
-{
-    MB_FC_READ_COILS,
-    MB_FC_READ_DISCRETE_INPUT,
-    MB_FC_READ_REGISTERS,
-    MB_FC_READ_INPUT_REGISTER,
-    MB_FC_WRITE_COIL,
-    MB_FC_WRITE_REGISTER,
-    MB_FC_WRITE_MULTIPLE_COILS,
-    MB_FC_WRITE_MULTIPLE_REGISTERS
-};
-
-#define T35  5
-#define  MAX_BUFFER  64	//!< maximum size for the communication buffer in bytes
-
-/**
- * @class Modbus
- * @brief
- * Arduino class library for communicating with Modbus devices over
- * USB/RS232/485 (via RTU protocol).
- */
-class Modbus
-{
-private:
-    Stream *port; //!< Pointer to Stream class object (Either HardwareSerial or SoftwareSerial)
-    uint8_t u8id; //!< 0=master, 1..247=slave number
-    uint8_t u8txenpin; //!< flow control pin: 0=USB or RS-232 mode, >1=RS-485 mode
-    uint8_t u8state;
-    uint8_t u8lastError;
-    uint8_t au8Buffer[MAX_BUFFER];
-    uint8_t u8BufferSize;
-    uint8_t u8lastRec;
-    uint16_t *au16regs;
-    uint16_t u16InCnt, u16OutCnt, u16errCnt;
-    uint16_t u16timeOut;
-    uint32_t u32time, u32timeOut, u32overTime;
-    uint8_t u8regsize;
-
-    void sendTxBuffer();
-    int8_t getRxBuffer();
-    uint16_t calcCRC(uint8_t u8length);
-    uint8_t validateAnswer();
-    uint8_t validateRequest();
-    void get_FC1();
-    void get_FC3();
-    int8_t process_FC1( uint16_t *regs, uint8_t u8size );
-    int8_t process_FC3( uint16_t *regs, uint8_t u8size );
-    int8_t process_FC5( uint16_t *regs, uint8_t u8size );
-    int8_t process_FC6( uint16_t *regs, uint8_t u8size );
-    int8_t process_FC15( uint16_t *regs, uint8_t u8size );
-    int8_t process_FC16( uint16_t *regs, uint8_t u8size );
-    void buildException( uint8_t u8exception ); // build exception message
-
-public:
-    Modbus(uint8_t u8id, Stream& port, uint8_t u8txenpin =0);
-
-    void start();
-    void setTimeOut( uint16_t u16timeOut); //!<write communication watch-dog timer
-    uint16_t getTimeOut(); //!<get communication watch-dog timer value
-    boolean getTimeOutState(); //!<get communication watch-dog timer state
-    int8_t query( modbus_t telegram ); //!<only for master
-    int8_t poll(); //!<cyclic poll for master
-    int8_t poll( uint16_t *regs, uint8_t u8size ); //!<cyclic poll for slave
-    uint16_t getInCnt(); //!<number of incoming messages
-    uint16_t getOutCnt(); //!<number of outcoming messages
-    uint16_t getErrCnt(); //!<error counter
-    uint8_t getID(); //!<get slave ID between 1 and 247
-    uint8_t getState();
-    uint8_t getLastError(); //!<get last error message
-    void setID( uint8_t u8id ); //!<write new ID for the slave
-    void setTxendPinOverTime( uint32_t u32overTime );
-    void end(); //!<finish any communication and release serial communication port
-
-    //
-    // Deprecated functions
-
-    // Deprecated: Use constructor: "Modbus m(0,Serial,0)" instead.
-    Modbus(uint8_t u8id=0, uint8_t u8serno=0, uint8_t u8txenpin=0) __attribute__((deprecated));
-
-    // Deprecated: Use "start()" instead.
-    template<typename T_Stream>
-    void begin(T_Stream* port_, long u32speed_) __attribute__((deprecated));
-
-    // Deprecated: Use "start()" instead.
-    template<typename T_Stream>
-    void begin(T_Stream* port_, long u32speed_, uint8_t u8txenpin_) __attribute__((deprecated));
-
-    // Deprecated: Use "start()" instead.
-    void begin(long u32speed = 19200) __attribute__((deprecated));
+{ = 19200) __attribute__((deprecated));
 };
 
 /* _____PUBLIC FUNCTIONS_____________________________________________________ */
@@ -504,16 +379,6 @@ uint16_t Modbus::getErrCnt()
  * Get modbus master state
  *
  * @return = 0 IDLE, = 1 WAITING FOR ANSWER
- * @ingroup buffer
- */
-uint8_t Modbus::getState()
-{
-    return u8state;
-}
-
-/**
- * Get the last error in the protocol processor
- *
  * @returnreturn   NO_REPLY = 255      Time-out
  * @return   EXC_FUNC_CODE = 1   Function code not available
  * @return   EXC_ADDR_RANGE = 2  Address beyond available space for Modbus registers
@@ -649,42 +514,7 @@ int8_t Modbus::poll()
         u8lastError = NO_REPLY;
         u16errCnt++;
         return 0;
-    }
-
-    if (u8current == 0) return 0;
-
-    // check T35 after frame end or still no frame end
-    if (u8current != u8lastRec)
-    {
-        u8lastRec = u8current;
-        u32time = millis();
-        return 0;
-    }
-    if ((unsigned long)(millis() -u32time) < (unsigned long)T35) return 0;
-
-    // transfer Serial buffer frame to auBuffer
-    u8lastRec = 0;
-    int8_t i8state = getRxBuffer();
-    if (i8state < 6) //7 was incorrect for functions 1 and 2 the smallest frame could be 6 bytes long
-    {
-        u8state = COM_IDLE;
-        u16errCnt++;
-        return i8state;
-    }
-
-    // validate message: id, CRC, FCT, exception
-    uint8_t u8exception = validateAnswer();
-    if (u8exception != 0)
-    {
-        u8state = COM_IDLE;
-        return u8exception;
-    }
-
-    // process answer
-    switch( au8Buffer[ FUNC ] )
-    {
-    case MB_FC_READ_COILS:
-    case MB_FC_READ_DISCRETE_INPUT:
+    }    case MB_FC_READ_DISCRETE_INPUT:
         // call get_FC1 to transfer the incoming message to au16regs buffer
         get_FC1( );
         break;
@@ -831,56 +661,7 @@ int8_t Modbus::getRxBuffer()
  * @brief
  * This method transmits au8Buffer to Serial line.
  * Only if u8txenpin != 0, there is a flow handling in order to keep
- * the RS485 transceiver in output state as long as the message is being sent.
- * This is done with UCSRxA register.
- * The CRC is appended to the buffer before starting to send it.
- *
- * @param nothing
- * @return nothing
- * @ingroup buffer
- */
-void Modbus::sendTxBuffer()
-{
-    // append CRC to message
-    uint16_t u16crc = calcCRC( u8BufferSize );
-    au8Buffer[ u8BufferSize ] = u16crc >> 8;
-    u8BufferSize++;
-    au8Buffer[ u8BufferSize ] = u16crc & 0x00ff;
-    u8BufferSize++;
-
-    if (u8txenpin > 1)
-    {
-        // set RS485 transceiver to transmit mode
-        digitalWrite( u8txenpin, HIGH );
-    }
-
-    // transfer buffer to serial line
-    port->write( au8Buffer, u8BufferSize );
-
-    if (u8txenpin > 1)
-    {
-        // must wait transmission end before changing pin state
-        // soft serial does not need it since it is blocking
-        // ...but the implementation in SoftwareSerial does nothing
-        // anyway, so no harm in calling it.
-        port->flush();
-        // return RS485 transceiver to receive mode
-        volatile uint32_t u32overTimeCountDown = u32overTime;
-        while ( u32overTimeCountDown-- > 0);
-        digitalWrite( u8txenpin, LOW );
-    }
-    while(port->read() >= 0);
-
-    u8BufferSize = 0;
-
-    // set time-out for master
-    u32timeOut = millis();
-
-    // increase message counter
-    u16OutCnt++;
-}
-
-/**
+ * the RS485 transceiver in output state as long as the messag*
  * @brief
  * This method calculates CRC
  *
